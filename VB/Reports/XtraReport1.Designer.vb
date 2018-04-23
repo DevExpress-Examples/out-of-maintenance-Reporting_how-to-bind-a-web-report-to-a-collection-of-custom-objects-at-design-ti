860926938eedfab3cfba818db4824494f61c44ab
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Reports/XtraReport1.Designer.vb
@@ -0,0 +1,107 @@
+﻿Namespace Reports
+    Partial Public Class XtraReport1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+            Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+            Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+            Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+            Me.xrLine1 = New DevExpress.XtraReports.UI.XRLine()
+            DirectCast(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' Detail
+            ' 
+            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLine1, Me.xrLabel2, Me.xrLabel1})
+            Me.Detail.Height = 42
+            Me.Detail.Name = "Detail"
+            Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' xrLabel2
+            ' 
+            Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Name", "")})
+            Me.xrLabel2.Location = New System.Drawing.Point(150, 0)
+            Me.xrLabel2.Name = "xrLabel2"
+            Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel2.Size = New System.Drawing.Size(100, 25)
+            Me.xrLabel2.Text = "xrLabel2"
+            ' 
+            ' xrLabel1
+            ' 
+            Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "ID", "")})
+            Me.xrLabel1.Location = New System.Drawing.Point(8, 0)
+            Me.xrLabel1.Name = "xrLabel1"
+            Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel1.Size = New System.Drawing.Size(100, 25)
+            Me.xrLabel1.Text = "xrLabel1"
+            ' 
+            ' PageHeader
+            ' 
+            Me.PageHeader.Height = 30
+            Me.PageHeader.Name = "PageHeader"
+            Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' PageFooter
+            ' 
+            Me.PageFooter.Height = 30
+            Me.PageFooter.Name = "PageFooter"
+            Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' bindingSource1
+            ' 
+            Me.bindingSource1.DataSource = GetType(Reports.Data)
+            ' 
+            ' xrLine1
+            ' 
+            Me.xrLine1.Location = New System.Drawing.Point(0, 33)
+            Me.xrLine1.Name = "xrLine1"
+            Me.xrLine1.Size = New System.Drawing.Size(650, 8)
+            ' 
+            ' XtraReport1
+            ' 
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter})
+            Me.DataSource = Me.bindingSource1
+            Me.Version = "8.1"
+            DirectCast(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+        End Sub
+
+        #End Region
+
+        Private Detail As DevExpress.XtraReports.UI.DetailBand
+        Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+        Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+        Private bindingSource1 As System.Windows.Forms.BindingSource
+        Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLine1 As DevExpress.XtraReports.UI.XRLine
+    End Class
+End Namespace
